e24bc85b638dca9b4ec5488d1c69bade8a627117
吳崇瑋 <lunary@st.tc.edu.tw>
2025-11-17 00:08:31 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js
+window.APP_CONFIG = {
+  // 請改成自己的 Google Apps Script Web App 網址（最後要 /exec）
+  WEB_APP_URL: "https://script.google.com/macros/s/AKfycbx_ls62nLUz9rP0wWpYetKBMerQnEuOid-uPqBycFayIi_Yp23EFnKnf_pI1h9yQM8/exec"
+};

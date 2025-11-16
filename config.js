14bda1dc7bd051474e59f51f91db1fc16c85d3cf
吳崇瑋 <lunary@st.tc.edu.tw>
2025-11-16 23:34:30 +0800

    Delete config.js

--- config.js
+++
@@ -1,6 +0,0 @@
-// =========================
-// 抽籤系統設定檔（可分享給其他老師用）
-// 只需修改這一行即可切換不同的 GAS Web App
-// =========================
-
-const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbx_ls62nLUz9rP0wWpYetKBMerQnEuOid-uPqBycFayIi_Yp23EFnKnf_pI1h9yQM8/exec";

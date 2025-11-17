183b47fb217f79d371c1c416d8b7888b1d785a53
吳崇瑋 <lunary@st.tc.edu.tw>
2025-11-17 22:11:55 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js
 window.APP_CONFIG = {
   // 請改成自己的 Google Apps Script Web App 網址（最後要 /exec）
-  WEB_APP_URL: "https://script.google.com/macros/s/AKfycbx_ls62nLUz9rP0wWpYetKBMerQnEuOid-uPqBycFayIi_Yp23EFnKnf_pI1h9yQM8/exec"
+  WEB_APP_URL: "https://script.google.com/macros/s/AKfycbyxdqQmuAU1jw43Ltw5nz8-3Izt3F0BXOQrzeLAdY7xBtSwzFEhtPnoJ4y9pakeedyy/exec"
 };
